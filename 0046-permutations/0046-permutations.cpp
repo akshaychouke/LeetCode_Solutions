378791ef28486b23961615869500379bc2a6b410
Akshay Chouke <akshaychauke50@gmail.com>
2023-07-02 23:07:34 +0530

    Time: 0 ms (100%), Space: 7.3 MB (99.82%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,38 +2,27 @@ class Solution {
 public:
 //this is the apporach 1 usign extra TC
 
-    void Solve(vector<int>& nums,vector<vector<int>> &ans,vector<int>& ds,int mpp[]){
+    void Solve(vector<int>& nums,vector<vector<int>> &ans,int index){
 
         //base condition
-        if(ds.size() == nums.size()){
-            ans.push_back(ds);
+        if(index == nums.size()){
+            ans.push_back(nums);
             return;
         }
 
-        for(int i=0;i<nums.size();i++){
-            if(mpp[i] != 1){
-             //to select element which is not selected and push in ds and mark true
-                ds.push_back(nums[i]);
-                mpp[i] = 1;
-                Solve(nums,ans,ds,mpp);  //recursive call 
-
-                // mark the element to 0 ans remove from ds for next permutation 
-                mpp[i] = 0; 
-                ds.pop_back();
-            }
+        //
+        for(int i=index;i<nums.size();i++){
+            //swap the numbers to ensure that every element is at possible position
+            swap(nums[i],nums[index]);
+            Solve(nums,ans,index+1);
+            swap(nums[i],nums[index]); //after coming back from recursion and then making array in its initail form
         }
+
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        int mpp[nums.size()];
-
-        for(auto i : mpp){
-            i = 0;
-        }
-
-        Solve(nums,ans,ds,mpp);
-
+        Solve(nums,ans,0);
         return ans;
-    }
+                
+        }
 };
