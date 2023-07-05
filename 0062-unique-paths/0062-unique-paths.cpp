e70ab8250f72de4c791a0eee04822cf8eeb157b6
Akshay Chouke <akshaychauke50@gmail.com>
2023-07-06 00:10:29 +0530

    Time: 0 ms (100%), Space: 6.5 MB (21.5%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-
-    int Solve(int m,int n,vector<vector<int>> &dp){
-
-        //base case 
-        if(m == 0 && n == 0) return 1;
-        if(m < 0 || n < 0) return 0;
-
-        //checl in dp
-        if(dp[m][n] != -1) return dp[m][n];
-
-        int up = Solve(m-1,n,dp);
-        int left = Solve(m,n-1,dp);
-
-        return dp[m][n] = up + left;
-    }
+//the tabulation solution
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
 
-        return Solve(m-1,n-1,dp);
+        dp[0][0] = 1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
+                if(i == 0 && j == 0) continue;
+
+                int up = 0,left = 0;
+                if(i > 0){
+                    up = dp[i-1][j];
+                }
+                if(j>0){
+                    left = dp[i][j-1];
+                }
+
+                dp[i][j] = up+left;
+            }
+        }
+
+        return dp[m-1][n-1];
+
     }
 };
