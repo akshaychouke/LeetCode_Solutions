bd9b657f32a6bb7ff2631ac031b5c2447260164b
Akshay Chouke <akshaychauke50@gmail.com>
2023-08-02 21:01:09 +0530

    Time: 0 ms (100%), Space: 7.6 MB (60.28%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-//this is the apporach 1 usign extra TC
 
-    void Solve(vector<int>& nums,vector<vector<int>> &ans,int index){
+    void Solve(vector<vector<int>> &ans,vector<int>& nums,int index){
 
-        //base condition
+        //base case
         if(index == nums.size()){
             ans.push_back(nums);
             return;
         }
 
-        //
+        //to take each element
         for(int i=index;i<nums.size();i++){
-            //swap the numbers to ensure that every element is at possible position
             swap(nums[i],nums[index]);
-            Solve(nums,ans,index+1);
-            swap(nums[i],nums[index]); //after coming back from recursion and then making array in its initail form
+            Solve(ans,nums,index+1);
+            swap(nums[i],nums[index]);  //backtrack
         }
-
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        Solve(nums,ans,0);
+        Solve(ans,nums,0);
         return ans;
-                
-        }
+    }
 };
