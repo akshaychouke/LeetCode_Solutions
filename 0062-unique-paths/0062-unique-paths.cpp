00d96ae92e54177600ad019043933d225bdfd9ae
Akshay Chouke <akshaychauke50@gmail.com>
2023-09-03 21:26:04 +0530

    Time: 0 ms (100%), Space: 6.7 MB (6.6%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-//the tabulation solution
+    int Solve(int a,int b,vector<vector<int>> &dp){
+        if(a < 0 || b < 0) return 0;
+        if(a == 0 && b == 0) return 1;
+
+        if(dp[a][b] != -1) return dp[a][b];
+
+        int up = Solve(a-1,b,dp);
+        int left = Solve(a,b-1,dp);
+
+        return dp[a][b] = up+left;
+    }
+
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
-
-        dp[0][0] = 1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-
-                if(i == 0 && j == 0) continue;
-
-                int up = 0,left = 0;
-                if(i > 0){
-                    up = dp[i-1][j];
-                }
-                if(j>0){
-                    left = dp[i][j-1];
-                }
-
-                dp[i][j] = up+left;
-            }
-        }
-
-        return dp[m-1][n-1];
-
+        return Solve(m-1,n-1,dp);
     }
 };
