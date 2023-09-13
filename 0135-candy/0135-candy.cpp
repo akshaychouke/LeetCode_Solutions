bf1f56037fd8ede4ea9f76cc86ee24845b9e589e
Akshay Chouke <akshaychauke50@gmail.com>
2023-09-13 11:50:51 +0530

    Time: 14 ms (83.4%), Space: 17.7 MB (72.88%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+
+        // Left to right pass
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // Right to left pass
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+                candies[i] = candies[i + 1] + 1;
+            }
+        }
+
+        int total_candies = 0;
+        for (int i = 0; i < n; i++) {
+            total_candies += candies[i];
+        }
+
+        return total_candies;
+    }
+};
