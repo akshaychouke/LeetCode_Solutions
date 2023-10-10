94b26612703407976dd3810454cdb5b935126b01
Akshay Chouke <akshaychauke50@gmail.com>
2023-10-10 23:04:55 +0530

    Time: 540 ms (5.04%), Space: 18.2 MB (5.43%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int Solve(int idx,vector<int>& nums,vector<int>& dp){
-        if(idx == nums.size()-1) return 0;
+        if(idx >= nums.size()-1) return 0;
         
 
         if(dp[idx] != INT_MAX-1) return dp[idx];
 
         for(int i=1;i<=nums[idx];i++){
-            if(idx+i < nums.size()){
-                int ans = Solve(idx+i,nums,dp)+1;
-                dp[idx] = min(dp[idx],ans);
-            }
+            int ans = Solve(idx+i,nums,dp)+1;
+            dp[idx] = min(dp[idx],ans);
         }
 
         return dp[idx];
