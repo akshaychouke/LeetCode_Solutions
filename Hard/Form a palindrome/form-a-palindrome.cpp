ceb7694dc80d1d5064c7af00fac24e6a0f9c213e
Akshay Chouke <akshaychauke50@gmail.com>
2023-12-26 01:15:22 +0530

    Updated Solution - GfG to GitHub

---
+++ Hard/Form a palindrome/form-a-palindrome.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+     int longestSubsequence(string s1, string s2) {
+      int n = s1.length();
+      int m = s2.length();
+    
+      vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+    
+      for (int i = 0; i <= n; i++) {
+        dp[i][0] = 0;
+      }
+    
+      for (int j = 0; j <= m; j++) {
+        dp[0][j] = 0;
+      }
+    
+      for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+          if (s1[i - 1] == s2[j - 1]) {
+            dp[i][j] = 1 + dp[i - 1][j - 1];
+          } else {
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+          }
+        }
+      }
+    
+      return dp[n][m];
+    }
+    int longestPalindromeSubsequence(string s) {
+      // Write your code here.
+      string t = s;
+      reverse(t.begin(), t.end());
+    
+      return longestSubsequence(s, t);
+    }
+    int countMin(string str){
+    //complete the function here
+        int n = str.length();
+        
+        return n - longestPalindromeSubsequence(str);
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.countMin(str) << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends
