5cde6f157c11086f56319843016825323ba3f13c
Akshay Chouke <akshaychauke50@gmail.com>
2024-01-19 22:12:53 +0530

    Updated Solution - GfG to GitHub

--- Medium/Peak element/peak-element.cpp
+++ Medium/Peak element/peak-element.cpp
@@ -14,15 +14,20 @@ class Solution
     public:
     int peakElement(int arr[], int n)
     {
-       // Your code here
-       if(arr[0] >= arr[1]) return 0;
-       if(arr[n-1] >= arr[n-2]) return n-1;
+       // Your code 
+       int s = 0,e=n-1;
        
-       for(int i=1;i<n-1;i++){
-           if(arr[i] > arr[i-1] && arr[i] > arr[i+1]) return i;
+       while(s < e){
+           int mid = s + (e-s)/2;
+           
+           if(arr[mid] <= arr[mid+1]){
+               s = mid+1;
+           }else{
+               e = mid;
+           }
        }
        
-       return -1;
+       return s;
     }
 };
 
