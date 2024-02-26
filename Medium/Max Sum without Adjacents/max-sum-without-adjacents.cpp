3804b5c657a961e8b6bf7668d99f626d4ed72426
Akshay Chouke <akshaychauke50@gmail.com>
2024-02-26 20:48:02 +0530

    Updated Solution - GfG to GitHub

--- Medium/Max Sum without Adjacents/max-sum-without-adjacents.cpp
+++ Medium/Max Sum without Adjacents/max-sum-without-adjacents.cpp
@@ -9,23 +9,22 @@ using namespace std;
 class Solution{
 public:	
 	// calculate the maximum sum with out adjacent
-    int Solve(vector<int>&arr,int ind,vector<int> &dp){
-        if(ind == 0) return arr[ind];
-         if(ind < 0) return 0;
-    
-         if(dp[ind] != -1) return dp[ind];
-         
-         int pick = arr[ind] + Solve(arr,ind-2,dp);
-         int notPick = 0 + Solve(arr,ind-1,dp);
-         
-         return dp[ind] = max(pick,notPick);
-         
-    }
-    
-	int findMaxSum(vector<int>&arr, int n) {
-	    // code here
-        vector<int> dp(n,-1);
-        return Solve(arr,n-1,dp);
+	int solve(int *arr, int n,int ind,vector<int> &dp){
+	    if(ind >= n){
+	        return 0;
+	    }
+	    
+	    if(dp[ind] != -1) return dp[ind];
+	    
+	    int take = arr[ind] + solve(arr,n,ind+2,dp);
+	    int nottake= solve(arr,n,ind+1,dp);
+	    
+	    return dp[ind] = max(take,nottake);
+	}
+	int findMaxSum(int *arr, int n) {
+	    // code 
+	    vector<int> dp(n+1,-1);
+	    return solve(arr,n,0,dp);
 	}
 };
 
@@ -37,7 +36,7 @@ int main() {
     while (t--) {
         int n;
         cin >> n;
-        vector<int>arr(n);
+        int arr[n];
         for (int i = 0; i < n; i++) {
             cin >> arr[i];
         }
