15741d311609028a78f4351952748ca6dd66ac95
Akshay Chouke <akshaychauke50@gmail.com>
2024-04-29 10:31:08 +0530

    Updated Solution - GfG to GitHub

--- Hard/Form a palindrome/form-a-palindrome.cpp
+++ Hard/Form a palindrome/form-a-palindrome.cpp
@@ -9,44 +9,29 @@ using namespace std;
 
 class Solution{
   public:
-     int longestSubsequence(string s1, string s2) {
-      int n = s1.length();
-      int m = s2.length();
+    int lcs(int i, int j, string &s, string t, vector<vector<int>> &dp) {
+      if (i < 0 || j < 0)
+        return 0;
     
-      vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+      if (dp[i][j] != -1)
+        return dp[i][j];
     
-      for (int i = 0; i <= n; i++) {
-        dp[i][0] = 0;
-      }
+      if (s[i] == t[j])
+        return dp[i][j] = 1 + lcs(i - 1, j - 1, s, t, dp);
     
-      for (int j = 0; j <= m; j++) {
-        dp[0][j] = 0;
-      }
-    
-      for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-          if (s1[i - 1] == s2[j - 1]) {
-            dp[i][j] = 1 + dp[i - 1][j - 1];
-          } else {
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-          }
-        }
-      }
-    
-      return dp[n][m];
+      return dp[i][j] = max(lcs(i - 1, j, s, t, dp), lcs(i, j - 1, s, t, dp));
     }
-    int longestPalindromeSubsequence(string s) {
-      // Write your code here.
-      string t = s;
-      reverse(t.begin(), t.end());
-    
-      return longestSubsequence(s, t);
-    }
+
     int countMin(string str){
     //complete the function here
-        int n = str.length();
-        
-        return n - longestPalindromeSubsequence(str);
+
+      int n = str.length();
+      string t = str;
+      reverse(t.begin(), t.end());
+      vector<vector<int>> dp(n, vector<int>(n, -1));
+      int paliLen =
+          lcs(n - 1, n - 1, str, t, dp); // length of longest palindromic subsquence
+      return n - paliLen;
     }
 };
 
